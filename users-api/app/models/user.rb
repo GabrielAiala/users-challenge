3bf7c702429f92876eb0e4220540989f8413cd90
Gabriel <gabriel.aiala@hotmail.com>
2024-10-07 08:45:35 -0300

    added email validation

--- users-api/app/models/user.rb
+++ users-api/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :addresses, dependent: :destroy 
   accepts_nested_attributes_for :addresses, allow_destroy: true
+
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
